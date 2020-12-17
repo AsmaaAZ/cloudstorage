a26961dfa9a13a37487bac85fb9ef3f49ca45dce
Asmaa Zomea <asma.a.z.asma@gmail.com>
2020-12-18 01:48:49 +0300

    .

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -19,180 +19,191 @@ import org.springframework.boot.web.server.LocalServerPort;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
-	@LocalServerPort
-	private int port;
+  @LocalServerPort
+  private int port;
 
-	private WebDriver driver;
+  private WebDriver driver;
 
-	@BeforeAll
-	static void beforeAll() {
-		WebDriverManager.firefoxdriver().setup();
-	}
+  @BeforeAll
+  static void beforeAll() {
+    WebDriverManager.firefoxdriver().setup();
+  }
 
-	@BeforeEach
-	public void beforeEach() {
-		this.driver = new FirefoxDriver();
-	}
+  @BeforeEach
+  public void beforeEach() {
+    this.driver = new FirefoxDriver();
+  }
 
-	@AfterEach
-	public void afterEach() {
-		if (this.driver != null) {
-			driver.quit();
-		}
-	}
+  @AfterEach
+  public void afterEach() {
+    if (this.driver != null) {
+      driver.quit();
+    }
+  }
 
-	//Test permit
-	@Order(1)
-	@Test
-	public void getLoginPageTest() {
-		driver.get("http://localhost:" + this.port + "/login");
-		Assertions.assertEquals("Login", driver.getTitle());
-	}
+  //Test permit
+  @Order(1)
+  @Test
+  public void getLoginPageTest() {
+    driver.get("http://localhost:" + this.port + "/login");
+    Assertions.assertEquals("Login", driver.getTitle());
+  }
 
-	//Test permit
-	@Order(2)
-	@Test
-	public void getSignupPageTest() {
-		driver.get("http://localhost:" + this.port + "/signup");
-		Assertions.assertEquals("Sign Up", driver.getTitle());
-	}
+  //Test permit
+  @Order(2)
+  @Test
+  public void getSignupPageTest() {
+    driver.get("http://localhost:" + this.port + "/signup");
+    Assertions.assertEquals("Sign Up", driver.getTitle());
+  }
 
-	//test unauthorized access
-	@Order(3)
-	@Test
-	public void getUnauthorizedHomePageTest() {
-		driver.get("http://localhost:" + this.port + "/home");
-		Assertions.assertNotEquals("Home", driver.getTitle());
-	}
+  //test unauthorized access
+  @Order(3)
+  @Test
+  public void getUnauthorizedHomePageTest() {
+    driver.get("http://localhost:" + this.port + "/home");
+    Assertions.assertNotEquals("Home", driver.getTitle());
+  }
 
-	//test unauthorized access
-	@Order(4)
-	@Test
-	public void getUnauthorizedResultPageTest(){
-		driver.get("http://localhost:" + this.port + "/result");
-		Assertions.assertNotEquals("Result", driver.getTitle());
-	}
+  //test unauthorized access
+  @Order(4)
+  @Test
+  public void getUnauthorizedResultPageTest() {
+    driver.get("http://localhost:" + this.port + "/result");
+    Assertions.assertNotEquals("Result", driver.getTitle());
+  }
 
-	//duplicate user not allowed test
-	@Order(5)
-	@Test
-	public void duplicatedUsersTest(){
-		SignupPageTests spt = new SignupPageTests(driver);
-		spt.duplicateUser(driver, port);
-		WebDriverWait wait = new WebDriverWait(driver, 5);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".alert.alert-danger")));
-		Assertions.assertEquals("Sign Up", driver.getTitle());
-	}
+  //duplicate user not allowed test
+  @Order(5)
+  @Test
+  public void duplicatedUsersTest() {
+    SignupPageTests spt = new SignupPageTests(driver);
+    spt.duplicateUser(driver, port);
+    WebDriverWait wait = new WebDriverWait(driver, 5);
+    wait.until(
+        ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".alert.alert-danger")));
+    Assertions.assertEquals("Sign Up", driver.getTitle());
+  }
 
-	//sign up, login, logout & verify logout
-	@Order(6)
-	@Test
-	public void doSignupLoginLogoutTest(){
-		driver.get("http://localhost:" + port + "/login");
-		LoginPageTests lpt = new LoginPageTests(driver);
-		WebDriverWait wait = new WebDriverWait(driver,5);
-		lpt.loginSignupLoginLogoutProcess(driver,port);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".alert.alert-dark")));
-		driver.get("http://localhost:" + port + "/home");
-		Assertions.assertEquals("Login", driver.getTitle());
-	}
+  //sign up, login, logout & verify logout
+  @Order(6)
+  @Test
+  public void doSignupLoginLogoutTest() {
+    driver.get("http://localhost:" + port + "/login");
+    LoginPageTests lpt = new LoginPageTests(driver);
+    WebDriverWait wait = new WebDriverWait(driver, 5);
+    lpt.loginSignupLoginLogoutProcess(driver, port);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".alert.alert-dark")));
+    driver.get("http://localhost:" + port + "/home");
+    Assertions.assertEquals("Login", driver.getTitle());
+  }
 
-	//Create a note & verify it's displayed
-	@Order(7)
-	@Test
-	public void notesCreationTest(){
-		String noteTitle = "";
-		driver.get("http://localhost:" + port + "/login");
-		LoginPageTests lpt = new LoginPageTests(driver);
-		lpt.notesLogin(driver, port);
-		HomePageTests hpt = new HomePageTests(driver);
-		hpt.addNote(driver);
-		driver.findElement(By.id("nav-notes-tab")).click();
-		List<WebElement> rows = driver.findElements(By.cssSelector("#userTable > tbody:nth-child(2) > tr:nth-child(1) > th:nth-child(2)"));
-		for (WebElement row : rows) {
-			noteTitle = row.getText();
-		}
-		Assertions.assertEquals("a note title", noteTitle);
-	}
+  //Create a note & verify it's displayed
+  @Order(7)
+  @Test
+  public void notesCreationTest() {
+    String noteTitle = "";
+    driver.get("http://localhost:" + port + "/login");
+    LoginPageTests lpt = new LoginPageTests(driver);
+    lpt.notesLogin(driver, port);
+    HomePageTests hpt = new HomePageTests(driver);
+    hpt.addNote(driver);
+    driver.findElement(By.id("nav-notes-tab")).click();
+    List<WebElement> rows = driver.findElements(
+        By.cssSelector("#userTable > tbody:nth-child(2) > tr:nth-child(1) > th:nth-child(2)"));
+    for (WebElement row : rows) {
+      noteTitle = row.getText();
+    }
+    Assertions.assertEquals("a note title", noteTitle);
+  }
 
-	//Edit and existing note and verify the edit
-	@Order(8)
-	@Test
-	public void notesEditTest(){
-		String noteTitle = "";
-		driver.get("http://localhost:" + port + "/login");
-		LoginPageTests lpt = new LoginPageTests(driver);
-		lpt.notesLogin(driver, port);
-		HomePageTests hpt = new HomePageTests(driver);
-		hpt.editNote(driver);
-		driver.findElement(By.id("nav-notes-tab")).click();
-		List<WebElement> rows = driver.findElements(By.cssSelector("#userTable > tbody:nth-child(2) > tr:nth-child(1) > th:nth-child(2)"));
-		for (WebElement row : rows) {
-			noteTitle = row.getText();
-		}
-		Assertions.assertEquals("an edited note title", noteTitle);
-	}
-	//Test to delete a note
-	@Order(9)
-	@Test
-	public void noteDeletionTest(){
-		boolean deletedNote;
-		driver.get("http://localhost:" + port + "/login");
-		LoginPageTests lpt = new LoginPageTests(driver);
-		lpt.notesLogin(driver, port);
-		HomePageTests hpt = new HomePageTests(driver);
-		hpt.deleteNote(driver);
-		driver.findElement(By.id("nav-notes-tab")).click();
-		List<WebElement> rows = driver.findElements(By.cssSelector("#userTable > tbody:nth-child(2) > tr:nth-child(1)"));
-		deletedNote = rows.size() < 1;
-		Assertions.assertTrue(deletedNote);
-	}
-	//Test creating credential and checking if password is encrypted
-	@Order(10)
-	@Test
-	public void createCredentialTest(){
-		driver.get("http://localhost:" + port + "/login");
-		LoginPageTests lpt = new LoginPageTests(driver);
-		lpt.credentialLogin(driver);
-		HomePageTests hpt = new HomePageTests(driver);
-		hpt.createCredential(driver);
+  //Edit and existing note and verify the edit
+  @Order(8)
+  @Test
+  public void notesEditTest() {
+    String noteTitle = "";
+    driver.get("http://localhost:" + port + "/login");
+    LoginPageTests lpt = new LoginPageTests(driver);
+    lpt.notesLogin(driver, port);
+    HomePageTests hpt = new HomePageTests(driver);
+    hpt.editNote(driver);
+    driver.findElement(By.id("nav-notes-tab")).click();
+    List<WebElement> rows = driver.findElements(
+        By.cssSelector("#userTable > tbody:nth-child(2) > tr:nth-child(1) > th:nth-child(2)"));
+    for (WebElement row : rows) {
+      noteTitle = row.getText();
+    }
+    Assertions.assertEquals("an edited note title", noteTitle);
+  }
 
-		String pwd = "Gooooo";
-		String encryptedPwd = driver.findElement(By.xpath("//table/tbody/tr[1]/td[3]")).getText();
-		System.out.println("encrypted password = " + encryptedPwd + "@@@@@@@@@@@@");
+  //Test to delete a note
+  @Order(9)
+  @Test
+  public void noteDeletionTest() {
+    boolean deletedNote;
+    driver.get("http://localhost:" + port + "/login");
+    LoginPageTests lpt = new LoginPageTests(driver);
+    lpt.notesLogin(driver, port);
+    HomePageTests hpt = new HomePageTests(driver);
+    hpt.deleteNote(driver);
+    driver.findElement(By.id("nav-notes-tab")).click();
+    List<WebElement> rows = driver
+        .findElements(By.cssSelector("#userTable > tbody:nth-child(2) > tr:nth-child(1)"));
+    deletedNote = rows.isEmpty();
+    Assertions.assertTrue(deletedNote);
+  }
 
-		Assertions.assertNotEquals(pwd, encryptedPwd);
-	}
+  //Test creating credential and checking if password is encrypted
+  @Order(10)
+  @Test
+  public void createCredentialTest() {
+    driver.get("http://localhost:" + port + "/login");
+    LoginPageTests lpt = new LoginPageTests(driver);
+    lpt.credentialLogin(driver);
+    HomePageTests hpt = new HomePageTests(driver);
+    hpt.createCredential(driver);
 
-	//test editing credentials, verify editing and making sure that the password is decrypted in the dialog
-	@Order(11)
-	@Test
-	public void editCredentialTest(){
-		String newUrl = "nothing";
-		driver.get("http://localhost:" + port + "/login");
-		LoginPageTests lpt = new LoginPageTests(driver);
-		lpt.credentialLogin(driver);
-		HomePageTests hpt = new HomePageTests(driver);
-		boolean isDecrypted = hpt.editCredential(driver);
-		if(isDecrypted){
-			newUrl = driver.findElement(By.cssSelector("#credentialTable > tbody:nth-child(2) > tr:nth-child(1) > th:nth-child(2)")).getAttribute("innerHTML");
-		}
-		Assertions.assertEquals("https://twitter.com/", newUrl);
-	}
+    String pwd = "Gooooo";
+    String encryptedPwd = driver.findElement(By.xpath("//table/tbody/tr[1]/td[3]")).getText();
+    System.out.println("encrypted password = " + encryptedPwd + "@@@@@@@@@@@@");
 
-	//test deleting credential and verifying it has been deleted
-	@Order(12)
-	@Test
-	public void deleteCredentialTest(){
-		boolean deletedCred;
-		driver.get("http://localhost:" + port + "/login");
-		LoginPageTests lpt = new LoginPageTests(driver);
-		lpt.credentialLogin(driver);
-		HomePageTests hpt = new HomePageTests(driver);
-		hpt.deleteCredential(driver);
-		driver.findElement(By.id("nav-credentials-tab")).click();
-		List<WebElement> rows = driver.findElements(By.cssSelector("#credentialTable > tbody:nth-child(2) > tr:nth-child(1)"));
-		deletedCred = rows.size() < 1;
-		Assertions.assertTrue(deletedCred);
-	}
+    Assertions.assertNotEquals(pwd, encryptedPwd);
+  }
+
+  //test editing credentials, verify editing and making sure that the password is decrypted in the dialog
+  @Order(11)
+  @Test
+  public void editCredentialTest() {
+    String newUrl = "nothing";
+    driver.get("http://localhost:" + port + "/login");
+    LoginPageTests lpt = new LoginPageTests(driver);
+    lpt.credentialLogin(driver);
+    HomePageTests hpt = new HomePageTests(driver);
+    boolean isDecrypted = hpt.editCredential(driver);
+    if (isDecrypted) {
+      newUrl = driver.findElement(By.cssSelector(
+          "#credentialTable > tbody:nth-child(2) > tr:nth-child(1) > th:nth-child(2)"))
+          .getAttribute("innerHTML");
+    }
+    Assertions.assertEquals("https://twitter.com/", newUrl);
+  }
+
+  //test deleting credential and verifying it has been deleted
+  @Order(12)
+  @Test
+  public void deleteCredentialTest() {
+    boolean deletedCred = false;
+    driver.get("http://localhost:" + port + "/login");
+    LoginPageTests lpt = new LoginPageTests(driver);
+    lpt.credentialLogin(driver);
+    HomePageTests hpt = new HomePageTests(driver);
+    hpt.deleteCredential(driver);
+    driver.findElement(By.id("nav-credentials-tab")).click();
+    List<WebElement> rows = driver
+        .findElements(By.xpath("//*[@id=\"credentialTable\"]/tbody/tr/td[contains('DuckDuck')]"));
+    System.out.println(rows + "@@@@rows@@@@@@");
+		deletedCred = rows.isEmpty();
+
+    Assertions.assertTrue(deletedCred);
+  }
 }
